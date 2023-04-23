0dca36ad9287943d120b0f3074dafac445aa4e5c
Natalia Stroganova <ntstroganova@gmail.com>
2023-04-23 22:26:32 +0300

    add filter_data and sort_data

--- utils/functions.py
+++ utils/functions.py
@@ -9,3 +9,15 @@ def get_data(json_file):
     with open(json_file, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
+
+
+def filter_data(data):
+    """Принимает данные и фильтрует их. Возвращает данные только с выполненными операциями"""
+    data = [x for x in data if 'state' in x and x['state'] == 'EXECUTED']
+    return data
+
+def sort_data(data):
+    """Сортирует данные по последней дате. Возвращает 5 последних операций """
+    data = sorted(data, key=lambda x: x['date'], reverse=True)
+    return data[0:5]
+
